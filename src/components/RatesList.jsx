17cc180773e4138da1c412f73949758a21fd39b3
Andrey Berg <andrey.berg.dev@gmail.com>
2019-02-11 09:08:07 +0500

    feat(RatesList): Добавлен поиск

--- src/components/RatesList.jsx
+++ src/components/RatesList.jsx
@@ -9,13 +9,40 @@ const propsTypes = {
 };
 
 class RatesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filter: '',
+    };
+  }
+
+  handlerOnUpdateFilter = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  filtredList = list => {
+    const { filter } = this.state;
+
+    if (!filter.length) return list;
+
+    return list.filter(item => item.name.indexOf(filter) >= 0);
+  };
+
   render() {
     const { list, favoritesAction, baseCurrency } = this.props;
 
     return (
       <section className="RatesList">
+        <div className="RatesList__search">
+          <input
+            type="text"
+            className="RatesList__search-field"
+            placeholder="Search..."
+            onChange={this.handlerOnUpdateFilter}
+          />
+        </div>
         <ul className="RatesList__list">
-          {list.map(item => (
+          {this.filtredList(list).map(item => (
             <li className="RatesList__list-item" key={item.ticker}>
               <ExchangeItem data={item} baseCurrency={baseCurrency} favoritesAction={favoritesAction} />
             </li>
